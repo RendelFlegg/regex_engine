86630b25dc01ed8a969d9007967dbd3ad91f536b
RendelFlegg <rendelflegg@ya.ru>
2022-05-12 12:48:26 +0300

    Completed Stage 2: Implemented multi character comparison

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,11 +1,17 @@
-data = input()
-regex, user_input = data.split('|')
+def reg_engine(data):
+    regex, user_input = data.split('|')
+    if '' in [regex, user_input]:
+        return regex == ''
+    elif single_character_check(regex[0], user_input[0]):
+        return reg_engine(f'{regex[1:]}|{user_input[1:]}')
+    else:
+        return False
 
-if regex == '':
-    print(True)
-elif user_input == '':
-    print(False)
-elif regex == '.':
-    print(True)
-else:
-    print(regex == user_input)
+
+def single_character_check(regex_character, user_character):
+    if regex_character == '.':
+        return True
+    return regex_character == user_character
+
+
+print(reg_engine(input()))
