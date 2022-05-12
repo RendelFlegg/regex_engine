9186943ddd04c997425c22e982ebee16603e9937
RendelFlegg <rendelflegg@ya.ru>
2022-05-12 11:23:48 +0300

    Completed Stage 1: Implemented one character comparison

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,1 +1,11 @@
-# write your code here
+data = input()
+regex, user_input = data.split('|')
+
+if regex == '':
+    print(True)
+elif user_input == '':
+    print(False)
+elif regex == '.':
+    print(True)
+else:
+    print(regex == user_input)
