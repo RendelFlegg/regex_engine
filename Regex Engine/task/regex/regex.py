cd8a7a5379195cb9c847e2c263c42a8033213a2c
RendelFlegg <rendelflegg@ya.ru>
2022-05-12 16:50:15 +0300

    Completed Stage 3: Implemented comparison of strings with different length

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,9 +1,13 @@
-def reg_engine(data):
+import sys
+sys.setrecursionlimit(10000)
+
+
+def multi_character_check(data):
     regex, user_input = data.split('|')
     if '' in [regex, user_input]:
         return regex == ''
     elif single_character_check(regex[0], user_input[0]):
-        return reg_engine(f'{regex[1:]}|{user_input[1:]}')
+        return multi_character_check(f'{regex[1:]}|{user_input[1:]}')
     else:
         return False
 
@@ -12,6 +16,16 @@ def single_character_check(regex_character, user_character):
     if regex_character == '.':
         return True
     return regex_character == user_character
+
+
+def reg_engine(data):
+    regex, user_input = data.split('|')
+    if multi_character_check(f'{regex}|{user_input}'):
+        return True
+    elif len(regex) > len(user_input):
+        return False
+    else:
+        return reg_engine(f'{regex}|{user_input[1:]}')
 
 
 print(reg_engine(input()))
