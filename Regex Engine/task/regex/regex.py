b2a580c8f6eabbe9ba7b72d961567f41ff833833
RendelFlegg <rendelflegg@ya.ru>
2022-05-18 18:56:13 +0300

    Completed stage 4: Implemented the operators ^ and $

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -20,6 +20,13 @@ def single_character_check(regex_character, user_character):
 
 def reg_engine(data):
     regex, user_input = data.split('|')
+    if regex.startswith('^') or regex.endswith('$'):
+        if regex.startswith('^') and regex.endswith('$'):
+            return regex[1:-1] == user_input
+        if regex.startswith('^'):
+            return reg_engine(f'{regex[1:]}|{user_input[:len(regex) - 1]}')
+        if regex.endswith('$'):
+            return reg_engine(f'{regex[:-1]}|{user_input[-len(regex) + 1:]}')
     if multi_character_check(f'{regex}|{user_input}'):
         return True
     elif len(regex) > len(user_input):
--- Regex Engine/task/regex/test_regex.py
+++ Regex Engine/task/regex/test_regex.py
@@ -16,13 +16,17 @@ class TestRegex(unittest.TestCase):  # a test case for the regex.py module
         self.assertFalse(regex.multi_character_check('peach|apple'), 'Error when comparing "peach|apple"')
 
     def test_reg_engine(self):
-        self.assertTrue(regex.reg_engine('apple|apple'), 'Error when comparing "apple|apple"')
-        self.assertTrue(regex.reg_engine('ap|apple'), 'Error when comparing ".ap|apple"')
-        self.assertTrue(regex.reg_engine('le|apple'), 'Error when comparing "le|apple"')
-        self.assertTrue(regex.reg_engine('a|apple'), 'Error when comparing "a|apple"')
-        self.assertTrue(regex.reg_engine('.|apple'), 'Error when comparing ".|apple"')
-        self.assertFalse(regex.reg_engine('apwle|apple'), 'Error when comparing "apwle|apple"')
-        self.assertFalse(regex.reg_engine('peach|apple'), 'Error when comparing "peach|apple"')
+        self.assertTrue(regex.reg_engine('^app|apple'), 'Error when comparing "^app|apple"')
+        self.assertTrue(regex.reg_engine('le$|apple'), 'Error when comparing "le$|apple"')
+        self.assertTrue(regex.reg_engine('^a|apple'), 'Error when comparing "^a|apple"')
+        self.assertTrue(regex.reg_engine('.$|apple'), 'Error when comparing ".$|apple"')
+        self.assertTrue(regex.reg_engine('apple$|tasty apple'), 'Error when comparing "apple$|tasty apple"')
+        self.assertTrue(regex.reg_engine('^apple|apple pie'), 'Error when comparing "^apple|apple pie"')
+        self.assertTrue(regex.reg_engine('^apple$|apple'), '^apple$|apple"')
+        self.assertFalse(regex.reg_engine('^apple$|tasty apple'), 'Error when comparing "^apple$|tasty apple"')
+        self.assertFalse(regex.reg_engine('^apple$|apple pie'), 'Error when comparing "^apple$|apple pie"')
+        self.assertFalse(regex.reg_engine('app$|apple'), 'Error when comparing "app$|apple"')
+        self.assertFalse(regex.reg_engine('^le|apple'), 'Error when comparing "^le|apple"')
 
 
 if __name__ == "__main__":
