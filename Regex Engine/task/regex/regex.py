ca79161cb87863c9f8bd28b3b552f0a174c73073
RendelFlegg <rendelflegg@ya.ru>
2022-05-20 15:15:15 +0300

    Some code brushing

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -2,9 +2,13 @@ import sys
 sys.setrecursionlimit(10000)
 
 
-def multi_character_check(data, backslash=False):
-    regex, user_input = data.split('|')
-    backslash = backslash
+def single_character_check(regex_character, user_character, backslash=False):
+    if regex_character == '.' and not backslash:
+        return True
+    return regex_character == user_character
+
+
+def multi_character_check(regex, user_input, backslash=False):
     if regex.startswith('\\') and not backslash:
         regex = regex[1:]
         backslash = True
@@ -15,33 +19,25 @@ def multi_character_check(data, backslash=False):
         if len(regex) > 1 and regex[1] in ['?', '*', '+'] and not backslash:
             operator = regex[1]
             if len(regex) > 2 and operator in ['?', '*', '+'] and regex[2] == user_input[0]:
-                return multi_character_check(f'{regex[2:]}|{user_input}', backslash=backslash)
+                return multi_character_check(regex[2:], user_input, backslash=backslash)
             if operator == '?':
                 operator_index = regex.index('?')
                 regex = regex.replace('?', '')
                 alt_regex = regex[:operator_index - 1] + regex[operator_index:]
-                return multi_character_check(f'{regex}|{user_input}', backslash=backslash) or multi_character_check(f'{alt_regex}|{user_input}', backslash=backslash)
+                return multi_character_check(regex, user_input, backslash=backslash) or multi_character_check(alt_regex, user_input, backslash=backslash)
             if regex[1] == '*':
-                return multi_character_check(f'{regex}|{user_input[1:]}', backslash=backslash)
+                return multi_character_check(regex, user_input[1:], backslash=backslash)
             alt_regex = regex.replace('+', '')
-            return multi_character_check(f'{regex}|{user_input[1:]}', backslash=backslash) or multi_character_check(f'{alt_regex[1:]}|{user_input[1:]}', backslash=backslash)
+            return multi_character_check(regex, user_input[1:], backslash=backslash) or multi_character_check(alt_regex[1:],user_input[1:], backslash=backslash)
 
-        return multi_character_check(f'{regex[1:]}|{user_input[1:]}', backslash=backslash)
+        return multi_character_check(regex[1:], user_input[1:], backslash=backslash)
     else:
         if len(regex) > 1 and regex[1] in ['?', '*']:
-            return multi_character_check(f'{regex[2:]}|{user_input}', backslash=backslash)
+            return multi_character_check(regex[2:], user_input, backslash=backslash)
         return False
 
 
-def single_character_check(regex_character, user_character, backslash=False):
-    if regex_character == '.' and not backslash:
-        return True
-    return regex_character == user_character
-
-
-def reg_engine(data, backslash=False):
-    regex, user_input = data.split('|')
-    backslash = backslash
+def reg_engine(regex, user_input, backslash=False):
     if regex.startswith('\\') and not backslash:
         regex = regex[1:]
         backslash = True
@@ -49,15 +45,16 @@ def reg_engine(data, backslash=False):
         if regex.endswith('$') and len(regex) == 2:
             return False
         if any(operator in regex for operator in ['?', '*', '+']):
-            return reg_engine(f'{regex[1:]}|{user_input}', backslash=backslash)
-        return multi_character_check(f'{regex[1:]}|{user_input}', backslash=backslash)
-    if multi_character_check(f'{regex}|{user_input}', backslash=backslash):
+            return reg_engine(regex[1:], user_input, backslash=backslash)
+        return multi_character_check(regex[1:], user_input, backslash=backslash)
+    if multi_character_check(regex, user_input, backslash=backslash):
         return True
     elif len(regex) > len(user_input):
         return False
     else:
-        return reg_engine(f'{regex}|{user_input[1:]}', backslash=backslash)
+        return reg_engine(regex, user_input[1:], backslash=backslash)
 
 
 if __name__ == "__main__":
-    print(reg_engine(input()))
+    pattern, user_text = input().split('|')
+    print(reg_engine(pattern, user_text))
